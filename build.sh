a02be9b925052acc5fcbe04fbb159b3fd827e505
Ayush Shrestha <ayushrashrest44@gmail.com>
2026-02-18 09:47:00 +0100

    Remove Pillow, force Python 3.11

--- build.sh
+++ build.sh
@@ -2,6 +2,12 @@
 # build.sh
 set -o errexit
 
+# Force Python 3.11
+export PYTHON_VERSION=3.11.0
+
+echo "Python version:"
+python --version
+
 echo "Installing dependencies..."
 pip install -r requirements.txt
 
